e7b68699f30a43ddd185999e3957c9015a499e2e
Brian Demant <brian.demant@gmail.com>
2016-05-27 03:10:19 +0200

    maybe it works

--- src/components/login/LoginComponent.js
+++ src/components/login/LoginComponent.js
@@ -1,30 +1,33 @@
 import React, {PropTypes} from 'react';
-import {Button} from '../../components/login/LoginComponent';
+
+import {Button,AdList} from '../../components';
+
 const propTypes = {
-    username: PropTypes.string.isRequired,
-    placeholder: PropTypes.string
+	username : PropTypes.string.isRequired,
+	placeholder : PropTypes.string
 };
 
 const defaultProps = {
-    placeholder: 'username'
+	placeholder : 'username'
 };
 
 export class Login extends React.Component {
-    render() {
-        return (
-            <div class="panel-body">
-                <fieldset>
-                    <div class="form-group">
-                        <input class="form-control" placeholder={this.props.placeholder} value={this.props.username} name="email" type="email" autofocus />
-                    </div>
-                    <div class="form-group">
-                        <input className="form-control" placeholder="Password" name="password" type="password"  />
-                    </div>
-                    <button type="submit" class="btn btn-lg btn-success btn-block">Login</button>
-                </fieldset>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div class="panel-body">
+				<fieldset>
+					<div class="form-group">
+						<input class="form-control" placeholder={this.props.placeholder} value={this.props.username}
+						       name="email" type="email" autofocus/>
+					</div>
+					<div class="form-group">
+						<input className="form-control" placeholder="Password" name="password" type="password"/>
+					</div>
+					<button type="submit" class="btn btn-lg btn-success btn-block">Login</button> 
+				</fieldset>
+			</div>
+		)
+	}
 }
 
 Login.propTypes = propTypes;
